28eb77cee12bad34ba7e79d07f17ce9a4a0389ef
BugHunter-X <askzenai@gmail.com>
2025-01-02 01:42:04 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,10 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let numbers = [1, 5, 2, 8, 3]
+  let sortedNumbers = sort numbers
+  print sortedNumbers
+```
+This code compiles and runs without errors, but doesn't demonstrate a bug.
